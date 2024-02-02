b12121f9d92a10ac9deb3137b581675278635e6f
Luis Hernandez <sarco_luis@hotmail.com>
2024-02-02 11:02:06 -0600

    Creacion de la rama conversor

--- controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -10,59 +10,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    RadioGroup opt;
-    Spinner spn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tempVal = findViewById(R.id.txtnum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
-
-                tempVal = findViewById(R.id.txtnum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
-
-                double respuesta = 0;
-                opt = findViewById(R.id.optOpciones);
-                spn = findViewById(R.id.spnOpciones);
-                switch (opt.getCheckedRadioButtonId()){
-                    case R.id.optSuma:
-                        respuesta = num1+num2;
-                        break;
-                    case R.id.optResta:
-                        respuesta = num1-num2;
-                        break;
-                    case R.id.optMulplicacion:
-                        respuesta = num1*num2;
-                        break;
-                    case R.id.optDivision:
-                        respuesta = num1/num2;
-                        break;
-                }
-                switch (spn.getSelectedItemPosition()){
-                    case 0:
-                        respuesta = num1+num2;
-                        break;
-                    case 1:
-                        respuesta = num1-num2;
-                        break;
-                    case 2:
-                        respuesta = num1*num2;
-                        break;
-                    case 3:
-                        respuesta = num1/num2;
-                        break;
-                }
-                tempVal = findViewById(R.id.lblrespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
-            }
-        });
     }
 }
