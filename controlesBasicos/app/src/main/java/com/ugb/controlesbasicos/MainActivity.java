fde2784c4e530d1af117b6722d294fb6035dd327
Luis Hernandez <sarco_luis@hotmail.com>
2024-02-12 11:09:44 -0600

    Creacion de la rama sensor_proximidad y programacion del sensor de proximidad

--- controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -2,6 +2,11 @@ package com.ugb.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,58 +16,56 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
-    Button btn;
-    RadioGroup opt;
-    Spinner spn;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
+        tempVal = findViewById(R.id.lblSensorProximidad);
+        activarSensorProximidad();
+    }
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void activarSensorProximidad(){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensor==null){
+            tempVal.setText("Tu telefono NO tiene sensor de proximidad");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
             @Override
-            public void onClick(View view) {
-                tempVal = findViewById(R.id.txtnum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
-
-                tempVal = findViewById(R.id.txtnum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
-
-                double respuesta = 0;
-                opt = findViewById(R.id.optOpciones);
-                spn = findViewById(R.id.spnOpciones);
-                switch (opt.getCheckedRadioButtonId()){
-                    case R.id.optSuma:
-                        respuesta = num1+num2;
-                        break;
-                    case R.id.optResta:
-                        respuesta = num1-num2;
-                        break;
-                    case R.id.optMulplicacion:
-                        respuesta = num1*num2;
-                        break;
-                    case R.id.optDivision:
-                        respuesta = num1/num2;
-                        break;
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                double valor = sensorEvent.values[0];
+                tempVal.setText("Proximidad: "+ valor);
+                if( valor<=4 ){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (valor<=8) {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }else{
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
                 }
-                switch (spn.getSelectedItemPosition()){
-                    case 0:
-                        respuesta = num1+num2;
-                        break;
-                    case 1:
-                        respuesta = num1-num2;
-                        break;
-                    case 2:
-                        respuesta = num1*num2;
-                        break;
-                    case 3:
-                        respuesta = num1/num2;
-                        break;
-                }
-                tempVal = findViewById(R.id.lblrespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
             }
-        });
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+    private void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
