13712a9bc3b4ea2676a57dfcbf738d9291bbbcbd
Luis Hernandez <sarco_luis@hotmail.com>
2024-02-12 12:44:42 -0600

    Creacion de la rama sensor_gps y programacion del sensor de gps

--- controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -1,71 +1,68 @@
 package com.ugb.controlesbasicos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
+    LocationManager locationManager;
+    LocationListener locationListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorLuz);
-        activarSensorLuz();
+        tempVal = findViewById(R.id.lblSensorGps);
+        obtenerUbicacion();
     }
-    @Override
-    protected void onResume() {
-        iniciar();
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-    private void activarSensorLuz(){
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if(sensor==null){
-            tempVal.setText("Tu telefono NO tiene sensor de Luz");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                double valor = sensorEvent.values[0];
-                tempVal.setText("Luz: "+ valor);
-                if( valor<=20 ){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                } else if (valor<=50) {
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                }else{
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                }
+    private void obtenerUbicacion(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            tempVal.setText("NO tienes permiso de acceder al GPS");
+            return;
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 1);
             }
+        }
+        /*Location location = new Location(LocationManager.GPS_PROVIDER);
+        mostrarUbicacion(location);*/
+        locationListener = new LocationListener() {
             @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("LocationUpdate", "Nueva ubicación: " + location.getLatitude() + ", " + location.getLongitude());
+                mostrarUbicacion(location);
             }
         };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
+    private void mostrarUbicacion(Location location){
+        tempVal.setText("Ubicacion: Altitud: "+ location.getAltitude() +"; Latitud: "+ location.getLatitude() + "; Longitud: "+ location.getLongitude());
     }
 }
