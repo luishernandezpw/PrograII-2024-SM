b3ff209aa68ddc1d60a590fb046fac4517c2183c
Luis Hernandez <sarco_luis@hotmail.com>
2024-03-15 10:09:30 -0600

    Se crearon los metodos necesarios para la toma de la foto

--- controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -1,11 +1,19 @@
 package com.ugb.controlesbasicos;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -13,12 +21,19 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     Button btn;
     FloatingActionButton fab;
     TextView tempVal;
     String accion = "nuevo";
     String id="";
+    String urlCompletaFoto;
+    Intent tomarFotoIntent;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,7 +81,56 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        img = findViewById(R.id.btnImgAmigo);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFotoAmigo();
+            }
+        });
         mostrarDatosAmigos();
+    }
+    private void tomarFotoAmigo(){
+        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File fotoAmigo = null;
+        try{
+            fotoAmigo = crearImagenAmigo();
+            if( fotoAmigo!=null ){
+                Uri uriFotoamigo = FileProvider.getUriForFile(MainActivity.this,
+                        "com.ugb.controlesbasicos.fileprovider", fotoAmigo);
+                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoamigo);
+                startActivityForResult(tomarFotoIntent, 1);
+            }else{
+                mostrarMsg("No se pudo creaar la foto");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al abrir la camara: "+ e.getMessage());
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if(resultCode==1 && resultCode==RESULT_OK){
+                Bitmap imageBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageBitmap(imageBitmap);
+            }else{
+                mostrarMsg("El usuario cancelo la toma de la foto");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error añ obtener la foto de la camara");
+        }
+    }
+    private File crearImagenAmigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
+            fileName = "imagen_"+ fechaHoraMs +"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if( dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdirs();
+        }
+        File imagen = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
+        urlCompletaFoto = imagen.getAbsolutePath();
+        return imagen;
     }
     private void mostrarDatosAmigos(){
         try{
