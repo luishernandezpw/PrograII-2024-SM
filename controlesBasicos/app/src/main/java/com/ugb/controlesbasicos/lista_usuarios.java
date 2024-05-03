717c6946e8bc1860b9742e49759a40eed499d931
Luis hernandez <sarco_luis@hotmail.com>
2024-05-03 10:31:38 -0600

    Configuracion de proyecto con firebase, y creacion de usuarios y lista de usuarios.

---
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/lista_usuarios.java
@@ -0,0 +1,14 @@
+package com.ugb.controlesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lista_usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_usuarios);
+    }
+}
